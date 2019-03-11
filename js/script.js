1a7c82c3308738455f667aafc9c380a1057782c8
thewhiteh4t <lohityapushkar@gmail.com>
2019-03-11 09:26:51 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -18,3 +18,29 @@ $.getJSON("https://ipapi.co/json/", function(data){
   $("#TZN").append(tzn);
   $("#ZIP").append(zip);
 });
+
+$(document).ready(function(){
+  var ua = navigator.userAgent;
+  var ver = navigator.appVersion;
+  var scw = window.screen.width;
+  var sch = window.screen.height;
+  var name = navigator.appCodeName;
+  var ptf = navigator.platform;
+  var cores = navigator.hardwareConcurrency;
+  var ram = navigator.deviceMemory;
+
+  gl = document.createElement('canvas').getContext('webgl');
+  debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+  var vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+  var render = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+  $("#VER").append(ver);
+  $("#SCW").append(scw);
+  $("#SCH").append(sch);
+  $("#NAME").append(name);
+  $("#PTF").append(ptf);
+  $("#CORES").append(cores);
+  $("#RAM").append(ram);
+  $("#VENDOR").append(vendor);
+  $("#RENDER").append(render);
+});
