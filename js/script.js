f633aa3944009adf68280d2c22b37b456b60817c
thewhiteh4t <lohityapushkar@gmail.com>
2019-03-11 07:40:05 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-$.getJSON("http://ip-api.com/json/?fields=205817", function(data){
+$.getJSON("https://ipapi.co/json/", function(data){
+  console.log(data)
   var cit = data.city;
-  var cnt = data.country;
-  var isp = data.isp;
-  var lat = data.lat;
-  var lon = data.lon;
-  var mob = data.mobile;
-  var Mob = String(mob);
-  var pxy = data.proxy;
-  var Pxy = String(pxy);
-  var ipa = data.query;
-  var rgn = data.regionName;
+  var cnt = data.country_name;
+  var isp = data.org;
+  var lat = data.latitude;
+  var lon = data.longitude;
+  var ipa = data.ip;
+  var rgn = data.region;
   var tzn = data.timezone;
-  var zip = data.zip;
+  var zip = data.postal;
   $("#IP").append(ipa);
   $("#ISP").append(isp);
   $("#CITY").append(cit);
