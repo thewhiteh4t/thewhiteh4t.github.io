7080237437a1102dc478ede08384e4487fff7f87
thewhiteh4t <lohityapushkar@gmail.com>
2025-08-28 09:04:34 +0530

    minor fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,9 @@ import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
-  const posts = await getCollection('blog')
+  const posts = (await getCollection('blog')).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+  )
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
